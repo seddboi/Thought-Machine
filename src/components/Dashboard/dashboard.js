5a4ad6adcd39fc8cd55d0b38b95828a0a56c083d
Gian Zamora <gz2016@att.net>
2021-11-27 23:54:53 -0800

    added dashboard and thought components as well as initial dashboard compnent edits

---
+++ src/components/Dashboard/dashboard.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Thought Machine</h1>
+        </div>
+    )
+};
+
+export default Dashboard;
